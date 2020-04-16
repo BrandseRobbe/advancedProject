81918bb6d6a3fa8f6c82c8240f040a5377366d69
Robbe <robbe.brandse@student.howest.be>
2020-04-16 17:23:46 +0200

    uservalidation

--- Models/User.py
+++ Models/User.py
@@ -1,6 +1,36 @@
+import re
+
+
 class User:
     def __init__(self, name, nickname, email, password):
         self.name = name
         self.nickname = nickname
         self.email = email
         self.password = password
+
+    @staticmethod
+    def isValidEmail(email):
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if re.search(regex, email):
+            return True
+        else:
+            return False
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, password):
+        self.__password = hash(password)
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        if self.isValidEmail(email):
+            self.__email = email
+        else:
+            raise ValueError
